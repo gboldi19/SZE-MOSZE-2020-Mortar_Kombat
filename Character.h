065c3a49a0e2fa7979b8547c037ccb13a8da2e26
Tóth Balázs <43994623+balazs482@users.noreply.github.com>
2020-10-12 13:13:39 +0200

    gotHit() now protected
    doHit() is now the only public function for attacks.

--- Character.h
+++ Character.h
@@ -12,11 +12,11 @@ protected:
 	const float getDMG() const;
 	static std::string* parseUnit(const std::string &fileName);
 	Character(const std::string* characterAttributes);
+	float gotHit(Character* attacker);
 public:
 	static Character CharacterFromFile(const std::string &fileName);
 	const std::string getName() const;
 	const float getHP() const;
-	float gotHit(Character* attacker);
 	virtual void doHit(Character& victim);
 };
 
