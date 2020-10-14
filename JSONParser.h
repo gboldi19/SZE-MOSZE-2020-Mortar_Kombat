6a2d1c5b8b65d73c6ab4c02b0c00ffba3f48f5f5
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-10-14 15:36:34 +0200

    JSONParser class added
    Added 3 type parsing (input, stream, file) with proper exception handling

---
+++ JSONParser.h
@@ -0,0 +1,26 @@
+#ifndef JSONPARSER_H
+#define JSONPARSER_H
+
+#include <map>
+#include <string>
+#include <any>
+#include <fstream>
+#include <streambuf>
+#include <unordered_set>
+
+class JSONParser
+{
+private:
+    //INSTRUCTIONS: Specify mode = "inclusive" for an allowed character set. Exclusive is default.
+    static std::string::size_type findNext(std::string &s, char target, std::unordered_set<char> set = {}, std::string mode = "");
+    static void checkString(std::string& s);
+    static std::any string2any(std::string& s);
+    static std::map<std::string, std::any> parseString(std::string& s);
+public:
+    //INSTRUCTIONS: Provide parseble string (default) or text file path (isFile must be set true)!
+    static std::map<std::string, std::any> parse(std::string inputString = "", bool isFile = false);
+    //INSTRUCTIONS: Provide parseble stream!
+    static std::map<std::string, std::any> parse(std::istream &stream);
+};
+
+#endif
