142b4a38381b15302be59c14a64bf13ff2c36036
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-10-15 23:31:45 +0200

    Removed public as its not a class

--- JSONParser.h
+++ JSONParser.h
@@ -11,7 +11,6 @@
 
 namespace JSONParser
 {
-public:
     //INSTRUCTIONS: Provide parseble string (default) or text file path (isFile must be set true)!
     static std::map<std::string, std::any> parse(std::string inputString = "", bool isFile = false);
     //INSTRUCTIONS: Provide parseble stream!
