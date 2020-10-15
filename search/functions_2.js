476ee2722448b4514cbb95fd7735f4a514a949f6
Parmama <1laurakiss1@gmail.com>
2020-10-15 15:00:39 +0000

    Update gh-pages to output generated at 38fe195

---
+++ search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gethp',['getHP',['../classCharacter.html#ab1f54566f8b68a8f8c5436b255050e50',1,'Character']]],
+  ['getname',['getName',['../classCharacter.html#a1ab1d31d7776d21a0315434acc61b342',1,'Character']]]
+];
