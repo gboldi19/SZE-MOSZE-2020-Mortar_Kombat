88321ff037996019c7c9a08a24ef6b96e398a766
balazs482 <balazs482@gmail.com>
2020-11-01 18:53:54 +0100

    Indentation fixed

--- unit_testing/json_parser_test.cpp
+++ unit_testing/json_parser_test.cpp
@@ -9,21 +9,21 @@
 TEST(jsonParserTest, parsetest)
 {
 	ASSERT_THROW(JSON::parse("none.json", true), std::runtime_error);
-    ASSERT_NO_THROW(JSON::parse("../units/unit1.json", true));
+   	ASSERT_NO_THROW(JSON::parse("../units/unit1.json", true));
 }
 
 TEST(jsonParserTest, valcheck)
 {
 	std::map<std::string, std::any> template_inp = JSON::parse("{\"string\":\"Stringtype\",\"bool\":true,\"float\":1.6}");
 	ASSERT_EQ(std::any_cast<std::string>(template_inp["string"]), "Stringtype");
-    ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
-    ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
+    	ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
+    	ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
 }
 
 TEST(jsonParserTest, filetest)
 {
 	std::ifstream jsonFile;
-    jsonFile.open("../unit_testing/missing_comma.json");
+	jsonFile.open("../unit_testing/missing_comma.json");
 	jsonFile.close();
 	ASSERT_THROW(JSON::parse(jsonFile), std::runtime_error);
 }
@@ -32,9 +32,9 @@ TEST(jsonParserTest, stringtest)
 {
 	std::map<std::string, std::any> template_string = JSON::parse("{\"name\":\"Kakarott\",\"hp\":380,\"dmg\":20,\"as\":1.2}");
 	ASSERT_EQ(std::any_cast<std::string>(template_string["name"]), "Kakarott");
-    ASSERT_EQ(std::any_cast<float>(template_string["hp"]), 380);
-    ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
-    ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
+	ASSERT_EQ(std::any_cast<float>(template_string["hp"]), 380);
+    	ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
+    	ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
 }
 
 TEST(jsonParserTest, missingfile)
