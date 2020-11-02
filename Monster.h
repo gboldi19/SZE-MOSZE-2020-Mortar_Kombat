72feebadac124621bd4a9221125198f97ac6d822
Tóth Balázs <43994623+balazs482@users.noreply.github.com>
2020-11-02 22:02:02 +0100

    Documentation update

--- Monster.h
+++ Monster.h
@@ -1,3 +1,8 @@
+/**
+ * @class Monster
+ * @brief This is a subclass representing the enemies.
+*/
+
 #ifndef MONSTER_H
 #define MONSTER_H
 
@@ -6,8 +11,13 @@
 class Monster : public Character
 {
 public:
-	Monster(const std::string characterName, float healthPoints, float damagePoints, float attackCoolDown);
-	static Monster parse(std::string fileName);
+    Monster(const std::string characterName, float healthPoints, float damagePoints, float attackCoolDown);
+    /**
+    * @brief Monster construction.
+    * @param Path to file containing the attributes.
+    * @return Monster with defined parameters.
+    */
+    static Monster parse(std::string fileName);
 };
 
 #endif
