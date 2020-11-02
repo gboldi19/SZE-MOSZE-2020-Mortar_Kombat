b2429f4a2f86ec1785aa07b5aa46d5ca41bc833b
balazs482 <balazs482@gmail.com>
2020-11-02 04:54:20 +0100

    New scenario test, workflows reworked.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'
-file='output.csv'
+file='output1.csv'
 
 ./a.out units/test_scenario1.json >> $file
 ./a.out units/test_scenario2.json >> $file
@@ -9,3 +9,8 @@ file='output.csv'
 ./a.out units/test_scenario4.json >> $file
 ./a.out units/test_scenario5.json >> $file
 ./a.out units/test_scenario6.json >> $file
+
+file='output2.csv'
+
+./a.out scenario1.json >> $file
+./a.out scenario2.json >> $file
