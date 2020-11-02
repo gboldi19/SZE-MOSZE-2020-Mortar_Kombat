b1ccee0ac3a11681584e3e7e21e63b0c5bfab544
balazs482 <balazs482@users.noreply.github.com>
2020-11-02 04:21:31 +0000

    Update gh-pages to output generated at 89d06fc

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['gethp',['getHP',['../classCharacter.html#af8adb5fabcbe6b8b8f04e0ee4a239935',1,'Character']]],
-  ['getname',['getName',['../classCharacter.html#a1ab1d31d7776d21a0315434acc61b342',1,'Character']]],
-  ['gothit',['gotHit',['../classCharacter.html#abed8c1bf3fae4ce36942dbf73a9d85d4',1,'Character']]]
+  ['hero',['Hero',['../classHero.html',1,'']]]
 ];
