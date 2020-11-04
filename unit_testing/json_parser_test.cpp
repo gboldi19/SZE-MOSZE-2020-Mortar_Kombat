ea1da0d3313c271eda28f9bd0527333350f6b77e
Rakonczai Róbert <1laurakiss1@gmail.com>
2020-11-04 21:59:50 +0100

    Fix json_parser_test.cpp

--- unit_testing/json_parser_test.cpp
+++ unit_testing/json_parser_test.cpp
@@ -42,8 +42,8 @@ TEST(jsonParserTest, valcheck)
 {
 	std::map<std::string, std::any> template_inp = JSON::parse("{\"string\":\"Stringtype\",\"bool\":true,\"float\":1.6,\"null pointer\":null}");
 	ASSERT_EQ(std::any_cast<std::string>(template_inp["string"]), "Stringtype");
-    ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
-    ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
+	ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
+	ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
 	ASSERT_EQ(std::any_cast<nullptr_t>(template_inp["null pointer"]), nullptr);
 }
 
@@ -60,8 +60,8 @@ TEST(jsonParserTest, stringtest)
 	std::map<std::string, std::any> template_string = JSON::parse("{\"name\":\"Kakarott\",\"hp\":380,\"dmg\":20,\"as\":1.2}");
 	ASSERT_EQ(std::any_cast<std::string>(template_string["name"]), "Kakarott");
 	ASSERT_EQ(std::any_cast<float>(template_string["hp"]), 380);
-    ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
-    ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
+	ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
+	ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
 }
 
 TEST(jsonParserTest, missingfile)
@@ -72,40 +72,40 @@ TEST(jsonParserTest, missingfile)
 
 TEST(jsonParserTest, hero_get_test)
 {
-	Hero hero = Hero::parse("../unit/unit1_Hero.json");
-	ASSERT_EQ(hero.getName(), "Kakarott");
-	ASSERT_EQ(hero.getMaxHealthPoints(), 380);
-	ASSERT_EQ(hero.getDamage(), 20);
-	ASSERT_EQ(hero.getAttackCoolDown(), 1.9);
+	Hero* hero = new Hero::parse("../unit/unit1_Hero.json");
+	ASSERT_EQ(hero->getName(), "Kakarott");
+	ASSERT_EQ(hero->getMaxHealthPoints(), 380);
+	ASSERT_EQ(hero->getDamage(), 20);
+	ASSERT_EQ(hero->getAttackCoolDown(), 1.9);
 }
 
 TEST(jsonParseTest, monster_get_test)
 {
-	Monster enemy = Monster::parse("Zombie.json");
-	ASSERT_EQ(enemy.getName(), "Zombie");
-	ASSERT_EQ(enemy.getHealthPoints(), 10);
-	ASSERT_EQ(enemy.getDamage(), 1);
-	ASSERT_EQ(enemy.getAttackCoolDown(), 2.8);
+	Monster* enemy = new Monster::parse("Zombie.json");
+	ASSERT_EQ(enemy->getName(), "Zombie");
+	ASSERT_EQ(enemy->getHealthPoints(), 10);
+	ASSERT_EQ(enemy->getDamage(), 1);
+	ASSERT_EQ(enemy->getAttackCoolDown(), 2.8);
 }
 
 TEST(jsonParseTest, type_test)
 {
-	Hero unit = Hero::parse("../unit/unit1_Hero.json");
-	ASSERT_EQ(typeid(std::string), typeid(unit.getName()));
-	ASSERT_EQ(typeid(float), typeid(unit.getHealthPoints()));
-	ASSERT_EQ(typeid(float), typeid(unit.getMaxHealthPoints()));
-	ASSERT_EQ(typeid(float), typeid(unit.getDamage));
-	ASSERT_EQ(typeid(float), typeid(unit.getAttackCoolDown()));
-	ASSERT_EQ(typeid(bool), typeid(unit.isAlive));
+	Hero* unit = new Hero::parse("../units/unit1_Hero.json");
+	ASSERT_EQ(typeid(std::string), typeid(unit->getName()));
+	ASSERT_EQ(typeid(float), typeid(unit->getHealthPoints()));
+	ASSERT_EQ(typeid(float), typeid(unit->getMaxHealthPoints()));
+	ASSERT_EQ(typeid(float), typeid(unit->getDamage()));
+	ASSERT_EQ(typeid(float), typeid(unit->getAttackCoolDown()));
+	ASSERT_EQ(typeid(bool), typeid(unit->isAlive()));
 
 }
 
 TEST(jsonParseTest, figthTilDeath_test)
 {
-	Hero hero = Hero::parse("../units/unit1_Hero.json");
-	Monster enemy = Monster::parse("../units/unit2_Monster.json");
-	hero.fightTilDeath(enemy);
-	ASSERT_FALSE(hero.isAlive() || enemy.isAlive());
+	Hero* hero = new Hero::parse("../units/unit1_Hero.json");
+	Monster* enemy = new Monster::parse("../units/unit2_Monster.json");
+	hero->fightTilDeath(enemy*);
+	ASSERT_FALSE(hero->isAlive() || enemy->isAlive());
 }
 
 TEST(jsonParserTest, missing_comma_test)
@@ -115,15 +115,15 @@ TEST(jsonParserTest, missing_comma_test)
 
 TEST(jsonParserTest, messed_up_input_keys_test)
 {
-	Hero bad_hero = Hero::parse("messed_up_input_keys.json");
-	ASSERT_EQ(bad_hero.getName(), "Kakarott");
-	ASSERT_EQ(bad_hero.getMaxHealthPoints(), 380);
-	ASSERT_EQ(bad_hero.getDamage(), 20);
-	ASSERT_EQ(bad_hero.getAttackCoolDown(), 1.9);
+	Hero* bad_hero = new Hero::parse("messed_up_input_keys.json");
+	ASSERT_EQ(bad_hero->getName(), "Kakarott");
+	ASSERT_EQ(bad_hero->getMaxHealthPoints(), 380);
+	ASSERT_EQ(bad_hero->getDamage(), 20);
+	ASSERT_EQ(bad_hero->getAttackCoolDown(), 1.9);
 }
+
 
 int main(int argc, char** argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+ n RUN_ALL_TESTS();
 }
