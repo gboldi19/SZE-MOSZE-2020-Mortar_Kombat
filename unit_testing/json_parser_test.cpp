f3cd4315c1814c9d48abaab65e6aca72d88cea39
Rakonczai Róbert <1laurakiss1@gmail.com>
2020-11-06 11:46:13 +0100

    Fix silly mistakes

--- unit_testing/json_parser_test.cpp
+++ unit_testing/json_parser_test.cpp
@@ -1,76 +1,50 @@
-#include "../JSON.h"
-#include "../Character.h"
-#include "../Hero.h"
-#include "../Monster.h"
-
+#include "../JSONParser.h"
 #include <gtest/gtest.h>
+
 #include <map>
 #include <string>
 #include <fstream>
 #include <any>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-int generateRandom() {
-	srand((unsigned)time(0));
-	int randomNumber = rand() % 1000;
-	return randomNumber;
-}
-
-std::string generateWhiteSapces()
-{
-	std::string whiteSpaces = "";
-	for (int i = 0; i < generateRandom(); i++) { whiteSpaces += " "; }
-	return whiteSpaces;
-}
-
-std::string generateCommas()
-{
-	std::string commas = "";
-	for (int i = 0; i < generateRandom(); i++) { commas += ","; }
-	return commas;
-}
 
 TEST(jsonParserTest, parsetest)
 {
-	ASSERT_THROW(JSON::parse("none.json", true), std::runtime_error);
-   	ASSERT_NO_THROW(JSON::parse("../unit_testing/good.json", true));
+	ASSERT_THROW(JSONParser::parse("none.json", true), std::runtime_error);
+    ASSERT_NO_THROW(JSONParser::parse("../units/unit1.json", true));
 }
 
 TEST(jsonParserTest, valcheck)
 {
-	std::map<std::string, std::any> template_inp = JSON::parse("{\"string\":\"Stringtype\",\"bool\":true,\"float\":1.6,\"null pointer\":null}");
+	std::map<std::string, std::any> template_inp = JSONParser::parse("{\"string\":\"Stringtype\",\"bool\":true,\"float\":1.6}");
 	ASSERT_EQ(std::any_cast<std::string>(template_inp["string"]), "Stringtype");
-	ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
-	ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
-	ASSERT_EQ(std::any_cast<nullptr_t>(template_inp["null pointer"]), nullptr);
+    ASSERT_EQ(std::any_cast<bool>(template_inp["bool"]), true);
+    ASSERT_EQ(std::any_cast<float>(template_inp["float"]), 1.6f);
 }
 
 TEST(jsonParserTest, filetest)
 {
 	std::ifstream jsonFile;
-	jsonFile.open("../unit_testing/missing_comma.json");
+    jsonFile.open("../unit_testing/missing_comma.json");
 	jsonFile.close();
-	ASSERT_THROW(JSON::parse(jsonFile), std::runtime_error);
+	ASSERT_THROW(JSONParser::parse(jsonFile), std::runtime_error);
 }
 
 TEST(jsonParserTest, stringtest)
 {
-	std::map<std::string, std::any> template_string = JSON::parse("{\"name\":\"Kakarott\",\"hp\":380,\"dmg\":20,\"as\":1.2}");
+	std::map<std::string, std::any> template_string = JSONParser::parse("{\"name\":\"Kakarott\",\"hp\":380,\"dmg\":20,\"as\":1.2}");
 	ASSERT_EQ(std::any_cast<std::string>(template_string["name"]), "Kakarott");
-	ASSERT_EQ(std::any_cast<float>(template_string["hp"]), 380);
-	ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
-	ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
+    ASSERT_EQ(std::any_cast<float>(template_string["hp"]), 380);
+    ASSERT_EQ(std::any_cast<float>(template_string["dmg"]), 20);
+    ASSERT_EQ(std::any_cast<float>(template_string["as"]), 1.2f);
 }
 
 TEST(jsonParserTest, missingfile)
 {
 	std::string not_exists = "../../tobe_or_nottobe_unit_test.json";
-	ASSERT_THROW(JSON::parse(not_exists, true), std::runtime_error);
+	ASSERT_THROW(JSONParser::parse(not_exists, true), std::runtime_error);
 }
 
 int main(int argc, char** argv)
 {
- RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
